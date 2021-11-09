ab1008c68a830e0b47a952259553a3a54e923562
유정무 <yujeongmu@yujeongmuui-MacBookPro.local>
2021-11-10 01:12:23 +0900

    add image and research button

--- src/screens/Research/ResearchScreen.js
+++ src/screens/Research/ResearchScreen.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
 import styled from 'styled-components/native';
 import { FlexCenterView, DefaultButton } from '../../components/Styles/styles';
+import ImageColors from 'react-native-image-colors';
 
 export default function ResearchScreen({ navigation }) {
 
     const ButtonHandler = () => {
-        navigation.navigate("Home")
+        navigation.navigate("Home");
+    }
+
+    const uri = require('../../images/cat.png');
+    
+
+    const ResearchButtonHandler = () => {
+
     }
 
     return (
         <FlexCenterView>
             <ContentText size={20}>ResearchScreen</ContentText>
+            <Img
+                source={require('../../images/cat.png')}
+            />
+            <DefaultButton
+                onPress={ResearchButtonHandler}
+            >
+                <ContentText
+                    size={15}
+                >
+                    Research
+                </ContentText>
+            </DefaultButton>
             <DefaultButton
                 onPress={ButtonHandler}
             >
@@ -26,5 +45,10 @@ export default function ResearchScreen({ navigation }) {
 }
 
 const ContentText = styled.Text`
-    font-size: ${props => props.size}
+    font-size: ${props => props.size};
+`
+
+const Img = styled.Image`
+    width: 150px;
+    height: 200px;
 `
