5fcd6195d21581d84d53f6e55bc0e5c85eca3d21
유정무 <yujeongmu@yujeongmuui-MacBookPro.local>
2021-11-11 01:48:32 +0900

    [#1] error occured

--- src/screens/Result/ResultScreen.js
+++ src/screens/Result/ResultScreen.js
@@ -6,9 +6,35 @@ import ImageColors from 'react-native-image-colors';
 
 export default function ResultScreen({ navigation }) {
 
+    const uri = require('./cat.png')
+
+    const result = await ImageColors.getColors(uri, {
+        fallback: '#228B22',
+        cache: true,
+        key: 'unique_key',
+    })
+
+    switch (result.platform) {
+        case 'android':
+            // android result properties
+            const vibrantColor = result.vibrant
+            break
+        case 'web':
+            // web result properties
+            const lightVibrantColor = result.lightVibrant
+            break
+        case 'ios':
+            // iOS result properties
+            const primaryColor = result.primary
+            break
+        default:
+            throw new Error('Unexpected platform key')
+    }
+
     return (
         <FlexCenterView>
             <ContentText size={20}>ResultScreen</ContentText>
+            <ContentText size={20}>{result}</ContentText>
         </FlexCenterView>
     );
 }
