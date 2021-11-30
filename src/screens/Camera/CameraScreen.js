5d7ddb34c3ced4f9d18555b4e893ecf86bd1f671
유정무 <yujeongmu@yujeongmuui-MacBookPro.local>
2021-11-30 12:25:58 +0900

    camera function resolved [#2]

--- src/screens/Camera/CameraScreen.js
+++ src/screens/Camera/CameraScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Camera } from 'expo-camera';
 import styled from 'styled-components/native';
@@ -6,6 +6,7 @@ import styled from 'styled-components/native';
 export default function CameraScreen() {
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
+  const ref = useRef(null);
 
   useEffect(() => {
     (async () => {
@@ -14,26 +15,29 @@ export default function CameraScreen() {
     })();
   }, []);
 
+  const takePicture = async () => {
+      const photo = await ref.current.takePictureAsync();
+      console.debug(photo);
+  };
+
   if (hasPermission === null) {
     return <View />;
   }
+
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
+
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
+      <Camera style={styles.camera} type={type} ref={ref}>
         <View style={styles.buttonContainer}>
           <TouchableOpacity
             style={styles.button}
             onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
+              takePicture();
             }}>
-            <Text style={styles.text}> Flip </Text>
+            <Text style={styles.text}> Snapp </Text>
           </TouchableOpacity>
         </View>
       </Camera>
@@ -42,27 +46,26 @@ export default function CameraScreen() {
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    camera: {
-      flex: 1,
-    },
-    buttonContainer: {
-      flex: 1,
-      justifyContent: 'flex-end',
-      backgroundColor: 'transparent',
-      flexDirection: 'row',
-      margin: 20,
-    },
-    button: {
-      flex: 0.1,
-      alignSelf: 'flex-end',
-      alignItems: 'center',
-    },
-    text: {
-      fontSize: 18,
-      color: 'white',
-    },
-  });
-  
+  container: {
+    flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
+  },
+  button: {
+    flex: 0.2,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
+  },
+});
